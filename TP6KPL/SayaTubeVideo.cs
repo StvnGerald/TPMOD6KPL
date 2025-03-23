2cb26c8d69c73975f304b015002cea6958d686be
Steven Gerald <stevengerald9924@gmail.com>
2025-03-24 02:45:46 +0700

    menambahkan class SayaTubeVideo

---
+++ TP6KPL/SayaTubeVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6KPL
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Steven Gerald");
+
+            video.IncreasePlayCount(100);
+            video.PrintVideoDetails();
+        }
+    }
+}
--- TP6KPL/obj/Debug/net8.0/TP6KPL.AssemblyInfo.cs
+++ TP6KPL/obj/Debug/net8.0/TP6KPL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP6KPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c0b47a9ea9412f5165a07e9fe8f5fa141cf5f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP6KPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP6KPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
